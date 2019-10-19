12262bd26edd218b5e667e994ada1e793dc63e5f
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-10-19 17:12:28 +0200

    Fix docs

---
+++ docs/precache-manifest.5c8bdfede450c1bc1eac4281ae7202d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d716618521137a52362339e268209a09",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ce53722b8c89ddce5af",
+    "url": "./static/css/main.13e741f6.chunk.css"
+  },
+  {
+    "revision": "292c27cea51ca59a6f0a",
+    "url": "./static/js/2.f20af6b0.chunk.js"
+  },
+  {
+    "revision": "6ce53722b8c89ddce5af",
+    "url": "./static/js/main.5a2d3431.chunk.js"
+  },
+  {
+    "revision": "9c246069fe98f25021e9",
+    "url": "./static/js/runtime-main.5795ef6a.js"
+  },
+  {
+    "revision": "a9ee2a5bdadecfa6248a9106158b803f",
+    "url": "./static/media/github_logo.a9ee2a5b.png"
+  },
+  {
+    "revision": "6a906af4a9eddf84772e827f4a637464",
+    "url": "./static/media/pokemon_logo.6a906af4.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ef0e436efbfba8cc3586f101dd8e160.js"
+  "./precache-manifest.5c8bdfede450c1bc1eac4281ae7202d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
