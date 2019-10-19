8c144caf5c3c6756a5feebafe55c2c74ea550fc8
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-10-19 15:14:35 +0200

    Run docs

---
+++ docs/precache-manifest.de44850d34c610b292e84660c6124179.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac0e00ee0d3d974ffaa277e07f16c96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdf415322d05880cdf59",
+    "url": "/static/css/main.c33cf3a7.chunk.css"
+  },
+  {
+    "revision": "bb37730a4e9afe232f96",
+    "url": "/static/js/2.91be5884.chunk.js"
+  },
+  {
+    "revision": "bdf415322d05880cdf59",
+    "url": "/static/js/main.2c6f073a.chunk.js"
+  },
+  {
+    "revision": "ceb4d14c33db407d2881",
+    "url": "/static/js/runtime-main.d06fc56f.js"
+  },
+  {
+    "revision": "a9ee2a5bdadecfa6248a9106158b803f",
+    "url": "/static/media/github_logo.a9ee2a5b.png"
+  },
+  {
+    "revision": "6a906af4a9eddf84772e827f4a637464",
+    "url": "/static/media/pokemon_logo.6a906af4.png"
+  }
+]);
